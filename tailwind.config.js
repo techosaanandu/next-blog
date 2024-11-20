422531c8d4d1dc5ca4ebaf0c89c81b50806f51f3
techosaanandu <techosaanandu@gmail.com>
2024-11-20 14:42:20 +0530

    second comit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+import flowbite  from "flowbite-react/tailwind"
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite-react/**/*.{js,ts,jsx,tsx}", // Include flowbite-react content
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -13,5 +17,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin"), // Correct way to include the Flowbite plugin
+  ],
 };
